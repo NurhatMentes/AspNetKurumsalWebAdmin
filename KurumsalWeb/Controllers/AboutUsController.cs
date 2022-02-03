ec2d1dae09eb026166ecf5a95631271e459942d4
Nurhat Menteş <nurhatmentes@gmail.com>
2022-02-03 15:21:56 +0300

    .

---
+++ KurumsalWeb/Controllers/AboutUsController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using KurumsalWeb.Models.DataContext;
+using KurumsalWeb.Models.Model;
+
+namespace KurumsalWeb.Controllers
+{
+    public class AboutUsController : Controller
+    {
+        private KurumsalDBContext db = new KurumsalDBContext(); 
+        // GET: AboutUs
+        public ActionResult Index()
+        {
+            var aboutUs = db.AboutUs.ToList();
+            return View(aboutUs);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var aboutUs = db.AboutUs.Where(x => x.AboutUsId == id).FirstOrDefault();
+            return View(aboutUs);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ValidateInput(false)]
+        public ActionResult Edit(int id, AboutUs aboutUs)
+        {
+            if (ModelState.IsValid)
+            {
+                var about = db.AboutUs.Where(x => x.AboutUsId == id).FirstOrDefault();
+                about.Description = aboutUs.Description;
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            return View(aboutUs);
+        }
+    }
+}
---
+++ KurumsalWeb/Controllers/ServiceController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.Entity;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+using KurumsalWeb.Models.DataContext;
+using KurumsalWeb.Models.Model;
+
+namespace KurumsalWeb.Controllers
+{
+    public class ServiceController : Controller
+    {
+        private KurumsalDBContext db = new KurumsalDBContext();
+        // GET: Service
+        public ActionResult Index()
+        {
+            return View(db.Services.ToList());
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult Create(Service service, HttpPostedFileBase ImgUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                if (ImgUrl != null)//önceden resim varsa sil
+                {
+                    WebImage image = new WebImage(ImgUrl.InputStream);
+                    FileInfo fileInfo = new FileInfo(ImgUrl.FileName);
+
+                    string imgName = Guid.NewGuid() + fileInfo.Extension;
+                    image.Resize(500, 500);
+                    image.Save("~/Uploads/Service/" + imgName);
+
+                    service.ImgUrl = "/Uploads/Service/" + imgName;
+                }
+
+                db.Services.Add(service);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(service);
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                ViewBag.Warning = "Güncellenecek hizmet bulunamadı.";
+            }
+
+            var service = db.Services.Find(id);
+
+            if (service==null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(service);
+        }
+
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult Edit(int? id, Service service, HttpPostedFileBase ImgUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                var serviceId = db.Services.Where(x => x.ServiceId == id).SingleOrDefault();
+                if (ImgUrl != null)
+                {
+                    if (System.IO.File.Exists(Server.MapPath(serviceId.ImgUrl)))
+                    {
+                        System.IO.File.Delete((Server.MapPath((serviceId.ImgUrl))));
+                    }
+
+                    WebImage image = new WebImage(ImgUrl.InputStream);
+                    FileInfo fileInfo = new FileInfo(ImgUrl.FileName);
+
+                    string imgName = Guid.NewGuid() + fileInfo.Extension;
+                    image.Resize(500, 500);
+                    image.Save("~/Uploads/Service/" + imgName);
+
+                    serviceId.ImgUrl = "/Uploads/Service/" + imgName;
+                }
+
+                serviceId.Description = service.Description;
+                serviceId.Title = service.Title;
+
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public ActionResult Delete(int id)
+        {
+            {
+                
+            }
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+
+            var serviceId = db.Services.Find(id);
+
+            if (serviceId == null)
+            {
+                return HttpNotFound();
+            }
+
+
+            db.Services.Remove(serviceId);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
