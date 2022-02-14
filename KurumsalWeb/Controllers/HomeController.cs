6fa648b448cbb1876ee64e0c118b72e726ac7042
Nurhat Menteş <nurhatmentes@gmail.com>
2022-02-14 13:15:33 +0300

    ..

--- KurumsalWeb/Controllers/HomeController.cs
+++ KurumsalWeb/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Helpers;
 using System.Web.Mvc;
 using KurumsalWeb.Models.DataContext;
 
@@ -13,8 +14,7 @@ namespace KurumsalWeb.Controllers
         // GET: Home
         public ActionResult Index()
         {
-            ViewBag.Contact = db.Contacts.SingleOrDefault();
-            ViewBag.Blog = db.Blogs.ToList().OrderByDescending(x => x.BlogId);
+            //Service ana sayfada da lazım olduğundan burada var
             ViewBag.Service = db.Services.ToList().OrderByDescending(x => x.ServiceId);
 
             return View();
@@ -28,6 +28,51 @@ namespace KurumsalWeb.Controllers
         public ActionResult ServicePartial()
         {
             return View(db.Services.ToList().OrderByDescending(x => x.ServiceId));
+        }
+
+        public ActionResult AboutUs()
+        {
+            return View(db.AboutUs.SingleOrDefault());
+        }
+
+        public ActionResult Service()
+        {
+            return View(db.Services.ToList().OrderByDescending(x => x.ServiceId));
+        }
+
+        public ActionResult Contact()
+        {
+            return View(db.Contacts.SingleOrDefault());
+        }
+
+        [HttpPost]
+        public ActionResult Contact(string email = null, string subject = null, string message = null, string name = null)
+        {
+            if (name != null && email != null && subject != null && message != null)
+            {
+                WebMail.SmtpServer = "smtp.gmail.com";
+                WebMail.EnableSsl = true;
+                WebMail.UserName = "strworktest@gmail.com";
+                WebMail.Password = "Test123.";
+                WebMail.SmtpPort = 587;
+                WebMail.Send("strworktest@gmail.com", subject, email + '\n' + message);
+                ViewBag.Danger = "Mesajınız başarı ile gönderilmiştir.";
+            }
+            else
+            {
+                ViewBag.Danger = "Hata oluştu. Tekrar deneyiniz.";
+            }
+
+            return View();
+        }
+
+        public ActionResult FooterPartial()
+        {
+            ViewBag.Contact = db.Contacts.SingleOrDefault();
+            ViewBag.Blog = db.Blogs.ToList().OrderByDescending(x => x.BlogId);
+            ViewBag.Service = db.Services.ToList().OrderByDescending(x => x.ServiceId);
+
+            return PartialView();
         }
     }
 }
